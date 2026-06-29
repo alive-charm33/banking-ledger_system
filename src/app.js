25699324da0a501ed9e5eefe1f7cb0925a957134
Sejal <bargalesejal@gmail.com>
2026-06-29 22:18:21 +0530

    feat: add user registration controller , Implemented the user registration endpoint with email existence verification to prevent duplicate account creation.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express=require('express');
-
+const authRouter=require("./routes/auth.routes")
 
 const app=express();
+//jitne bhi requredt /api/auth ko hit karenge wo sare ke sare authRouter pr redirect honnge
+//so hume authRoyter mein 2 endpoint banane honge...chalo auth.rotes.js file mein end point create krne
+app.use(express.json())//apna express ka server by default requrest.body ke andar ka data nahi padh sakta isyoye hum ye middleware use krte hia
+app.use("/api/auth",authRouter)
 module.exports=app
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+const userModel=require("../models/user.model")
+
+//user register controller and
+//POST /api/auth/register
+async function userRegisterController(req,res){
+//controller mein kuch data ayega wo data ke sath hume naya user create krna hoga
+// wo data kya kya ayega:email,pasword,name
+     const {email,password,name}=req.body
+     const isExists=await userModel.findOne({
+        email:email
+     })
+     if(isExists){
+        return res.status(422).json({
+            message:"User already existes with email",
+            status:"failed"
+        })
+     }
+}
+module.exports={
+    userRegisterController
+}
