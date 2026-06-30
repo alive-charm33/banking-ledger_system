9c546ee758369c9ee2b86bd3a24ef69f607a396a
Sejal <bargalesejal@gmail.com>
2026-06-30 18:37:21 +0530

    feat(auth): implement register and login controllers with Postman testing

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config()
 const app = require("./src/app");
 const connectToDB = require("./src/config/db");
 
+const PORT = process.env.PORT || 5000;
+
 async function start() {
     await connectToDB();
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
 }
 
--- test-a.js
+++
@@ -1,5 +0,0 @@
-const dns = require("dns");
-
-dns.resolve4("google.com", (err, addresses) => {
-  console.log(err || addresses);
-});
